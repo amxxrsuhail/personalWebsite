04bf86bac908cfaa9578b8c64680bc5dbec8e074
ameer suhail <kameer41099@gmail.com>
2023-01-01 19:53:01 +0530

    changes in repoURL

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/amxxrsuhail/personalWebsite.git";
+const repoURL = "https://github.com/amxxrsuhail/amxxrsuhail.github.io.git";
 
 ghpages.publish(
   pathname,
